c82569e5f7c83d4ea2b1287469399badc1004ec2
Austin Vichich <avichich4@gmail.com>
2023-10-10 23:31:47 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+      cats.push(name);
+  };
+  
+  destructivelyAppendCat("Ralph");
+  console.log(cats);
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+destructivelyPrependCat("Bob");
+console.log(cats);
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+
+};
+
+destructivelyRemoveLastCat("Ralph");
+console.log(cats);
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+
+};
+
+destructivelyRemoveFirstCat("Milo");
+console.log(cats);
+
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+  
+  console.log(appendCat("Broom"));
+  console.log(cats);
+
+function prependCat(name) {
+    const newCats = [name,...cats];
+    return newCats;
+  }
+  
+  console.log(prependCat("Arnold"));
+  console.log(cats);
+
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
+  console.log(removeLastCat());
+  console.log(cats);
+
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
+  console.log(removeFirstCat());
+  console.log(cats);
